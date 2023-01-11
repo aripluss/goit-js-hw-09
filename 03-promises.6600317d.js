547027d17b039b2b8577efa2d416bfb61c8e41e0
aripluss <116344378+aripluss@users.noreply.github.com>
2023-01-11 22:34:01 +0000

    Deploying to gh-pages from @ aripluss/goit-js-hw-09@074e2942f4bc060d633f6ba1bde5a192cbbf970b 🚀

--- 03-promises.6600317d.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form");e.addEventListener("submit",createPromise),console.log(e.amount)}();
-//# sourceMappingURL=03-promises.6600317d.js.map
--- 03-promises.9a690731.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");e.addEventListener("submit",createPromise),console.log(e.amount);
-//# sourceMappingURL=03-promises.9a690731.js.map
